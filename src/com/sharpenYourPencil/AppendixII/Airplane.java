0da553f54341e5ffbc98692623e4345b27f15a13
damanjits <damanjits@wolfram.com>
2018-03-30 08:58:09 -0500

    added appendixii examples on page 584

---
+++ src/com/sharpenYourPencil/AppendixII/Airplane.java
@@ -0,0 +1,17 @@
+package com.sharpenYourPencil.AppendixII;
+
+public class Airplane {
+    public int speed;
+    
+    public Airplane() {
+        
+    }
+    
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    public int getSpeed() {
+        return this.speed;
+    }
+}
---
+++ src/com/sharpenYourPencil/AppendixII/FlyTest2.java
@@ -0,0 +1,10 @@
+package com.sharpenYourPencil.AppendixII;
+
+public class FlyTest2 {
+    public static void main(String args[]) {
+        Airplane biplane = new Airplane();
+        biplane.speed = 212;
+        
+        System.out.println(biplane.speed);
+    }
+}
